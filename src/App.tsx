e5dcff564356327524233c2ddb8bad03b0554033
dgrigorev <dm-grig@mail.ru>
2021-12-20 16:28:42 +0300

    clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header name-class">
         <Counter />
         
       </header>
